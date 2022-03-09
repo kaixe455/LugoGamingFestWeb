4890c8e6271d7435fdfcae06971e4e4b8afa83e1
kaixe455 <48770955+kaixe455@users.noreply.github.com>
2022-03-09 21:28:12 +0100

    añadir patrocinadores - colaboradores
    Añadir patrocinadores y colaboradores.

--- src/app/services/invitados.service.ts
+++ src/app/services/invitados.service.ts
@@ -21,30 +21,53 @@ export class InvitadosService {
         "spotify":""
       },
       {"id":2,
-      "nombre":"HelanyaH",
-      "ocupacion":"Influencer",
-      "imagen":"./assets/fotos_invitados/helanyah.png",
-      "twitter":"HelanyaH",
-      "instagram":"HelanyaH",
+      "nombre":"DfreezeGIRL",
+      "ocupacion":"Embajadora",
+      "imagen":"./assets/fotos_invitados/DfreezeGIRL.png",
+      "twitter":"nereaandrade7",
+      "instagram":"nereaandrade7",
       "spotify":""
       },
       {"id":3,
-      "nombre":"DirectoD2",
-      "ocupacion":"Influencer",
-      "imagen":"./assets/fotos_invitados/D2.png",
-      "twitter":"DirectoD2",
-      "instagram":"DirectoD2",
+      "nombre":"Dualview",
+      "ocupacion":"Embajador",
+      "imagen":"./assets/fotos_invitados/Dualview.png",
+      "twitter":"dualviewyt",
+      "instagram":"dualview_yt",
       "spotify":""
       },
-      {"id":4,
+      { "id":4,
         "nombre":"SelediaCosplay",
         "ocupacion":"Cosplayer",
         "imagen":"./assets/fotos_invitados/Seledia.jpg",
         "twitter":"SelediaCosplay",
         "instagram":"SelediaCosplay",
         "spotify":""
+       },
+      { "id":5,
+        "nombre":"NewBeatOrder",
+        "ocupacion":"Productor & DJ",
+        "imagen":"./assets/fotos_invitados/NewBeatOrder.png",
+        "twitter":"",
+        "instagram":"NewBeatOrder",
+        "spotify":"0gQDOj4OisQbTq7FUEziea?si=vgtSq7B2Rt2SmQbYPbxPpQ"
+      },
+      { "id":6,
+        "nombre":"HelanyaH",
+        "ocupacion":"Influencer",
+        "imagen":"./assets/fotos_invitados/helanyah.png",
+        "twitter":"HelanyaH",
+        "instagram":"HelanyaH",
+        "spotify":""
+      },
+      { "id":7,
+        "nombre":"DirectoD2",
+        "ocupacion":"Influencer",
+        "imagen":"./assets/fotos_invitados/D2.png",
+        "twitter":"DirectoD2",
+        "instagram":"DirectoD2",
+        "spotify":""
       }
-
     ]
   }
 }
